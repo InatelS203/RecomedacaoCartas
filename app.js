31477734d0bf56687a9b572b3cbe40a6d89830a8
juliocloud <juliocesarjuriolli@gmail.com>
2024-09-19 21:09:06 -0300

    feat: initializing db

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
+const pool = require('./db')
 const port = 3000;
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+// Rota para inicializar o banco de dados
+app.get('/initialize-db', async (req, res) => {
+    try {
+        await pool.query('create database cardgame');
+    } catch (err){
+        console.log(err)
+        res.sendStatus(500)
+    }
+})
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
-const pool = new Pool()
+const pool = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'adm',
+    password: 'adm',
+    database: 'postgres'
+})
 
 module.exports = pool
