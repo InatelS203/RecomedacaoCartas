3a223980163dcc902b35b77e2230c70f6f96af1f
Henrique Corrêa <125215042+Chagaiz28@users.noreply.github.com>
2024-10-18 14:36:27 -0300

    Add exemplo de json de saída

---
+++ output.js
@@ -0,0 +1,26 @@
+{
+  "recommended_cards": [
+    {
+      "card_id": 123,
+      "name": "Dragon Slayer",
+      "type": "Attack",
+      "rarity": "Legendary",
+      "attributes": {
+        "damage": 50,
+        "mana_cost": 10
+      },
+      "description": "A powerful attack card for dealing high damage."
+    },
+    {
+      "card_id": 456,
+      "name": "Healing Potion",
+      "type": "Support",
+      "rarity": "Common",
+      "attributes": {
+        "heal_amount": 30,
+        "mana_cost": 5
+      },
+      "description": "Restores a moderate amount of health."
+    }
+  ]
+}
